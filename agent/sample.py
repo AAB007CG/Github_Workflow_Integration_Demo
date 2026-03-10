5adc6183874bdddb11b72c514ebba9d7024fc68c
AAB007CG <akash.benki@capgemini.com>
2026-03-10 11:49:42 +0530

    Modified sample.py

--- agent/sample.py
+++ agent/sample.py
@@ -1,4 +1,5 @@
 def insecure():
     username = "admin"
     password = "admin123" # password is exposed intentionally
+    testinghere = "This is a test for success PR Validation"
     return eval("2 + 2")
