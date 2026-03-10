238261744a3a82ef8a4faac7af535dd3fdac02ba
AAB007CG <akash.benki@capgemini.com>
2026-03-10 12:04:02 +0530

    Updated the sample.py

--- agent/sample.py
+++ agent/sample.py
@@ -2,4 +2,5 @@ def insecure():
     username = "admin"
     password = "admin123" # password is exposed intentionally
     justtestinghere = "This is for Failure case testing only"
+    justtestinghere2 = "This is for Failure case testing only"
     return eval("2 + 2")
