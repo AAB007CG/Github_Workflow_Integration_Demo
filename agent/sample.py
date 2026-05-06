b54e90b403d4f2006b09a9fcd94698e966c683dc
AAB007CG <akash.benki@capgemini.com>
2026-05-06 10:56:11 +0530

    Testing

--- agent/sample.py
+++ agent/sample.py
@@ -1,5 +1,4 @@
 def insecure():
     username = "admin"
     password = "admin123" # password is exposed intentionally
-    test = "This is a test string with a potential vulnerability."
     return eval("2 + 2")
